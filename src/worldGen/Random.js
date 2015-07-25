86dad6936026c42c46fa3fb71e030704f4b8b942
Rez <rezmodeus@yahoo.com>
2015-07-25 13:08:19 +0200

    worldseed added to random

--- src/worldGen/Random.js
+++ src/worldGen/Random.js
@@ -2,17 +2,23 @@
 module.exports = function() {
 	'use strict';
 
+	var worldSeed = '';
 	var seedRandom = require('seed-random');
 	function getUniqueFunction(seed){
-		return seedRandom(seed);
+		return seedRandom(worldSeed+seed);
+	}
+	function setWorldSeed(seed){
+		worldSeed = seed;
 	}
 
 	var api = {
-		getUniqueFunction:getUniqueFunction
+		getUniqueFunction:getUniqueFunction,
+		setWorldSeed:setWorldSeed
 	};
 	//removeIf(production)
 	api._test = {};
 	api._test.getUniqueFunction = getUniqueFunction;
+	api._test.setWorldSeed = setWorldSeed;
 	//endRemoveIf(production)
 
 	return api;
--- test/test-Random.js
+++ test/test-Random.js
@@ -15,7 +15,7 @@ describe('Random',function(){
 		expect(random).to.be.ok;
 	});
 
-	it('getNextFunction values', function(){
+	it('should return that are unique', function(){
 		var f1 = random.getUniqueFunction('testA');
 		var f2 = random.getUniqueFunction('testB');
 		expect(f1()).to.not.equal(f2());
@@ -26,6 +26,14 @@ describe('Random',function(){
 		var f2 = random.getUniqueFunction('test');
 		expect(f1()).to.equal(f2());
 		expect(f1()).to.equal(f2());
+	});
+
+	it('should use world seed',function(){
+		random.setWorldSeed('a');
+		var f1 = random.getUniqueFunction('test');
+		random.setWorldSeed('b');
+		var f2 = random.getUniqueFunction('test');
+		expect(f1()).to.not.equal(f2());
 	});
 
 
